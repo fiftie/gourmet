f42776ea2080696896b316e6aa813a1c0fa6c487
yanatori <yanatori333@gmail.com>
2021-06-01 22:43:36 +0900

    todoSlice-fix

--- src/provider/todoSlice.js
+++ src/provider/todoSlice.js
@@ -2,30 +2,26 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const todoSlice = createSlice({
   name: 'todo',
-  initialState: {
-    allState:{
+  initialTodoState: {
+    todo:{
       name: "",
       tel: "",
       streetAddress: "",
       memo: "",
       category: "",
-    }
+    },
+    todoState:{
+      todos:[]
+    },
   },
   reducers: {
-    onClickadd: () => {
-      if (allState === "") return;
-      const newAllState = [...allState,{
-        id: allState.length,
-        name: name,
-        tel: tel,
-        streetAddress: streetAddress,
-        memo: memo
-      }];
-      setAllState(newAllState);
+    // 現在のtodosの末尾に1つtodoを追加する。
+    pushTodo(todoState,PayloadAction) {
+      todos = [...todos, todo];
     },
   },
 })
 
-export const { onClickadd } = counterSlice.actions
+export const { onClickadd } = todoSlice.actions
 
-export default counterSlice.reducer
+export default todoSlice.reducer
